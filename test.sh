fb9f76d563974265c1d77b2fd0d63679fa7d521b
JingYu <92147862+iiEpoch@users.noreply.github.com>
2023-02-09 10:24:10 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ mkdir ${report_path}
 disable_wal="false"
 
 			
-data_path=
+data_path="/mnt/nvme3"
 dataset=200
 value=256
 cp -r ${data_path}/${value}B/${dataset}G/* ${test_path}
@@ -23,7 +23,14 @@ echo "进行测试写${dataset}G-value${value}-Disable_Wal-${disable_wal}"
 
 ./overwrite.sh ${test_path} ${dataset} ${value} ${report_path} ${disable_wal}
 		
+echo "sync测试"
 
+rm -rf ${test_path}
+
+cp -r /mnt/nvme3/256B/200G/* ${test_path}
+echo "sync"
+./overwrite_sync_off.sh ${test_path} 200 256 ${report_path} "false"
+rm -rf ${test_path}
 sleep 5
 
 echo "本次测试完成删除test目录"
