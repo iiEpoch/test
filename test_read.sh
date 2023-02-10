8f67b4515c1c3c4cd9e980df3f67b1d639312a0d
JingYu <92147862+iiEpoch@users.noreply.github.com>
2023-02-10 14:57:02 +0800

    Update test_read.sh

--- test_read.sh
+++ test_read.sh
@@ -6,8 +6,8 @@ read -p "report_path:" report_path
 mkdir ${report_path}
 
 
-#duration(s)
-duration=720
+#reads
+reads=5000000
 
 
 for value in {256,512,1024,4096}
@@ -31,10 +31,9 @@ do
 	for dataset in {200,400,600}
 	do		
 		
-		for read_cache_size_ratio in {0,25,50}
+		for read_cache_size in {25,50,100}
 		do
-			read_cache_size=$((${dataset}*${read_cache_size_ratio}))
-			echo read_cache_size_ratio=${read_cache_size_ratio}
+
 			echo "创建测试目录${test_path}"
 			mkdir ${test_path}
 
@@ -44,7 +43,7 @@ do
 			sleep 10
 	
 			echo "进行测试读${dataset}G-value${value}-cache${read_cache_size}"
-			./read.sh ${test_path} ${dataset} ${value} ${report_path} ${read_cache_size} ${duration}
+			./read.sh ${test_path} ${dataset} ${value} ${report_path} ${read_cache_size} ${reads}
 			sleep 10
 
 
